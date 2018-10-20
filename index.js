9be51735a64e3ab92cf8c0e9a3e70f692e18d87c
scorpionakki <alkesh302@gmail.com>
2018-10-20 19:02:38 +0530

    Signup Details are added to the database

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-var username_value =  document.getElementById("username").value;
-    var email_value = document.getElementById("email").value;
-    var password_value = document.getElementById("password").value;
-    var profpicName_value = document.getElementById("phone").value;
-    var database = firebase.database().ref();
-    var description_value = document.getElementById("description").value;
+var firebaseRef = firebase.database().ref('users');
+function signupinsertion(){
+    
+    var username_val = document.getElementById('username').value;
+    var email_val = document.getElementById('email').value;
+    var phone_val = document.getElementById('phone').value;
+    var password_val = document.getElementById('password').value;
+    var profpic_FileName_val = document.getElementById('profpic_FileName').value;
+    var profpic_FileName_val_split = profpic_FileName_val.split("\\");
+    var profpic_FileName_val_split_filename = profpic_FileName_val_split[profpic_FileName_val_split.length - 1];
+    var description_val = document.getElementById('description').value;
 
-    function signupdata() {
-        database.set({
-            email: email_value,
-            password : password_value,
-            phone : profpicName_value,
-            description : description_value
-        });
-    }
+
+    firebaseRef.child(username_val).child('details').set({
+        email : email_val,
+        phone : phone_val,
+        password : password_val,
+        profpic_FileName : profpic_FileName_val_split_filename,
+        description : description_val
+    });
+
+    window.alert('Inserted');
+}
