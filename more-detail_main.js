1707f79876dd8e1cdddc28ec0f24171e73d8b711
scorpionakki <alkesh302@gmail.com>
2018-10-22 22:01:38 +0530

    added ids to the textboxs that was removed due to Github Conflict

--- more-detail_main.js
+++ more-detail_main.js
@@ -7,6 +7,7 @@ var full_type = url_split[2];
 var full_type_split = full_type.split("=");
 var final_type = full_type_split[1];
 
+
 var fetchreminderRef = firebase.database().ref('users').child('testing_reig').child(final_type).child(final_id);
 
 fetchreminderRef.on('value',function(snap){
@@ -19,20 +20,20 @@ fetchreminderRef.on('value',function(snap){
 });
 
 
-function updatevalues(){
+// function updatevalues(){
 
-    var title_updated = document.getElementById('more_detail_title').value;
-    var description_updated = document.getElementById('more_detail_description').value;
-    var date_updated = document.getElementById('more_detail_date').value;
-    var time_updated = document.getElementById('more_detail_time').value;
-    var category_updated = document.getElementById('more_detail_category').value;
+//     var title_updated = document.getElementById('more_detail_title').value;
+//     var description_updated = document.getElementById('more_detail_description').value;
+//     var date_updated = document.getElementById('more_detail_date').value;
+//     var time_updated = document.getElementById('more_detail_time').value;
+//     var category_updated = document.getElementById('more_detail_category').value;
 
-    fetchreminderRef.set({
-        title : title_updated,
-        content : description_updated,
-        date : date_updated,
-        time : time_updated,
-        category : category_updated
-    });
+//     fetchreminderRef.set({
+//         title : title_updated,
+//         content : description_updated,
+//         date : date_updated,
+//         time : time_updated,
+//         category : category_updated
+//     });
 
-}
+// }
