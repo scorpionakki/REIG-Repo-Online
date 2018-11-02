6c3c72e74b0bf7b208fec53885f82b267aaa600c
scorpionakki <alkesh302@gmail.com>
2018-11-02 10:59:17 +0530

    Temporary Added done - Link to add Left

--- JS_Files_DB/contant-page-alkesh.js
+++ JS_Files_DB/contant-page-alkesh.js
@@ -1,39 +1,23 @@
-var databaseRef = firebase.database().ref('users');
+var databaseRef = firebase.database();
 
 function getusername(){
+
+    var input_value = document.getElementById('search_value').value;
+
+    databaseRef = databaseRef.ref('users/'+input_value);
     
-    
-    var getvalue = document.getElementById('search_value').value;
     databaseRef.on('value',function(snap){
-        
-        var values = snap.val();
-        var keys = Object.keys(values);        
-        var flag = 0;
-        for(i=0;i<keys.length;i++)
-        {
-            if(getvalue == keys[i])
-            {
-                var ulx = document.getElementById('myUL');
-                ulx.setAttribute("class","list-group checked-list-box");
-                var li = document.createElement('li');
-                li.setAttribute("class","list-group-item");
-                li.appendChild(document.createTextNode(keys[i]));
-                ulx.appendChild(li);
-                //document.getElementById('search_value').value = "Found";
-                break;            
-            }
-            else
-            {
-                flag++;        
-            }
-        }
-        if(flag==keys.length)
-        {
-            document.getElementById('search_value').value = "Not Found";  
-        }
-        
+
+        var table = document.getElementById("table_searchresults");
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+
+        cell1.innerHTML = snap.key;
+        cell2.innerHTML = "ADD - Temporary Text";
+
+
     });
-    
     
     
 }
