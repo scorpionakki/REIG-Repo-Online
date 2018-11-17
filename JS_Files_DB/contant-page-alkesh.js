8e0e5aa837da3c8f16d24e3d15de6d1d73522a6e
scorpionakki <alkesh302@gmail.com>
2018-11-17 23:53:02 +0530

    Updated the add contacts part
    Now group creation is left

--- JS_Files_DB/contant-page-alkesh.js
+++ JS_Files_DB/contant-page-alkesh.js
@@ -1,10 +1,62 @@
-window.alert('Just trying commit');
-
+var databaseRef = firebase.database().ref().child('users');
 function getusername(){
     
     var input_value = document.getElementById('search_value').value;
     
-    var databaseRef = firebase.database().ref('users/'+input_value);
+    // databaseRef = databaseRef.ref('users');
+    databaseRef.on('value',function(snap){
+        var users = snap.val();
+        var keys = Object.keys(users);
+        
+        for(var i = 0; i< keys.length;i++){
+            databaseRef.child(keys[i]).child('details').child('email').on('value',function(email){
+                if(input_value ==  email.val()){
+                    var table = document.getElementById("table_searchresults");
+                    if(document.getElementById('table_searchresults').rows.length > 1){
+                        for(var i = 1;i<=document.getElementById('table_searchresults').table.rows.length;i++){
+                            document.getElementById('table_searchresults').deleteRow(i);
+                        }
+                    }
+                    else
+                    {
+                        var row = table.insertRow(1);
+                        var cell1 = row.insertCell(0);
+                        var cell2 = row.insertCell(1);
+                        
+                        
+                        
+                        var alink_more_details = document.createElement("button");
+                        var alink_more_details_text = document.createTextNode('ADD');
+                        alink_more_details.appendChild(alink_more_details_text);
+                        // alink.setAttribute("href","http://index.html");
+                        //alink_more_details.setAttribute('value',"ADD")
+                        
+                        alink_more_details.setAttribute('onclick',"add_member_in_table_addedmembers(); return false");
+                        alink_more_details.setAttribute('id',"add_member_btn");
+                        //alink_more_details.href = "more-detail.html?id="+id;
+                        
+                        // alink_more_details.href = "#table_addedmembers?id="+id;
+                        
+                        
+                        cell1.innerHTML = email.val();
+                        cell2.appendChild(alink_more_details);
+                        
+                        var hidden_field = document.createElement("INPUT");
+                        hidden_field.setAttribute("type","hidden");
+                        hidden_field.setAttribute("id","hidden_value");
+                        document.body.appendChild(hidden_field);
+                        
+                        
+                        document.getElementById('hidden_value').value = email.val();
+                        
+                        // }
+                        //document.getElementById('table_searchresults').deleteRow(table.rows.length - 1); 
+                    }  
+                }
+            });
+        }
+        
+    });
     
     var table_rows = document.getElementById('table_addedmembers');
     
@@ -28,46 +80,7 @@ function getusername(){
         }
         else
         {
-            var table = document.getElementById("table_searchresults");
-            // if(document.getElementById('table_searchresults').rows.length > 1){
-            //     for(var i = 1;i<=document.getElementById('table_searchresults').table.rows.length;i++){
-            //         document.getElementById('table_searchresults').deleteRow(i);
-            //     }
-            // }
-            // else
-            // {
-            var row = table.insertRow(1);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
             
-            
-            
-            var alink_more_details = document.createElement("button");
-            var alink_more_details_text = document.createTextNode('ADD');
-            alink_more_details.appendChild(alink_more_details_text);
-            // alink.setAttribute("href","http://index.html");
-            //alink_more_details.setAttribute('value',"ADD")
-            
-            alink_more_details.setAttribute('onclick',"add_member_in_table_addedmembers(); return false");
-            alink_more_details.setAttribute('id',"add_member_btn");
-            //alink_more_details.href = "more-detail.html?id="+id;
-            
-            // alink_more_details.href = "#table_addedmembers?id="+id;
-            
-            
-            cell1.innerHTML = snap.key;
-            cell2.appendChild(alink_more_details);
-            
-            var hidden_field = document.createElement("INPUT");
-            hidden_field.setAttribute("type","hidden");
-            hidden_field.setAttribute("id","hidden_value");
-            document.body.appendChild(hidden_field);
-            
-            
-            document.getElementById('hidden_value').value = snap.key;
-            
-            // }
-            //document.getElementById('table_searchresults').deleteRow(table.rows.length - 1);
         }
         
         
@@ -75,7 +88,6 @@ function getusername(){
         
     });
     
-    //window.alert(databaseRef);
     
     
     
@@ -88,39 +100,39 @@ function add_member_in_table_addedmembers(){
     var row = table.insertRow(table.rows.length);
     var cell1 = row.insertCell(0);
     cell1.innerHTML = document.getElementById('hidden_value').value;
-    
-    
-    
-    
+
+
+
+
     // document.getElementById('add_member_btn').disabled= true;
     var table = document.getElementById("table_searchresults");
     table.deleteRow(1);
 }
 
-var databaseRef = firebase.database().ref('groups');
+// var databaseRef = firebase.database().ref('groups');
 
-function creategroup(){
-    
-   
-    var username = sessionStorage.getItem('username');
-    var group_pic = document.getElementById('group_pic').value;
-    var group_name = document.getElementById('group_name').value;
-    var group_description = document.getElementById('group_description').value;
-    
-    databaseRef.child(group_name).child('details').set({
-            description : group_description,
-            created_by : username
-    });
-    
-    var table = document.getElementById('table_addedmembers');
-    for(var i=1;i<table.rows.length;i++){
-        var value = table.rows[i].cells[0].innerHTML;
-        var member = "member"+i;
-        databaseRef.child(group_name).child('members').push({
-           member : value 
-        });
-    }
+// function creategroup(){
 
-   
-    
-}
+
+//     var username = sessionStorage.getItem('username');
+//     var group_pic = document.getElementById('group_pic').value;
+//     var group_name = document.getElementById('group_name').value;
+//     var group_description = document.getElementById('group_description').value;
+
+//     databaseRef.child(group_name).child('details').set({
+//         description : group_description,
+//         created_by : username
+//     });
+
+//     var table = document.getElementById('table_addedmembers');
+//     for(var i=1;i<table.rows.length;i++){
+//         var value = table.rows[i].cells[0].innerHTML;
+//         var member = "member"+i;
+//         databaseRef.child(group_name).child('members').push({
+//             member : value 
+//         });
+//     }
+
+
+
+// }
