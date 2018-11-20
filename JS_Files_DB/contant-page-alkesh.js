9f989d0dbefdf8dd182f624a3839f2388eade570
scorpionakki <alkesh302@gmail.com>
2018-11-20 11:25:28 +0530

    Working with Group Members & Group Creation
    Not fully ready

--- JS_Files_DB/contant-page-alkesh.js
+++ JS_Files_DB/contant-page-alkesh.js
@@ -1,97 +1,174 @@
 var databaseRef = firebase.database().ref().child('users');
-function getusername(){
-    
-    var input_value = document.getElementById('search_value').value;
-    
-    // databaseRef = databaseRef.ref('users');
-    databaseRef.on('value',function(snap){
-        var users = snap.val();
-        var keys = Object.keys(users);
-        
-        for(var i = 0; i< keys.length;i++){
-            databaseRef.child(keys[i]).child('details').child('email').on('value',function(email){
-                if(input_value ==  email.val()){
-                    var table = document.getElementById("table_searchresults");
-                    if(document.getElementById('table_searchresults').rows.length > 1){
-                        for(var i = 1;i<=document.getElementById('table_searchresults').table.rows.length;i++){
-                            document.getElementById('table_searchresults').deleteRow(i);
+var databaseRef_group = firebase.database().ref().child('groups');
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        document.getElementById('search_member_btn').onclick = function(){
+            var input_value = document.getElementById('search_value').value;
+            
+            // databaseRef = databaseRef.ref('users');
+            databaseRef.on('value',function(snap){
+                var users = snap.val();
+                var keys = Object.keys(users);
+                
+                for(var i = 0; i< keys.length;i++){
+                    databaseRef.child(keys[i]).child('details').child('email').on('value',function(email){
+                        if(input_value ==  email.val()){
+                            var table = document.getElementById("table_searchresults");
+                            if(document.getElementById('table_searchresults').rows.length > 1){
+                                for(var i = 1;i<=document.getElementById('table_searchresults').table.rows.length;i++){
+                                    document.getElementById('table_searchresults').deleteRow(i);
+                                }
+                            }
+                            else
+                            {
+                                var row = table.insertRow(1);
+                                var cell1 = row.insertCell(0);
+                                var cell2 = row.insertCell(1);
+                                
+                                
+                                
+                                var alink_more_details = document.createElement("button");
+                                var alink_more_details_text = document.createTextNode('ADD');
+                                alink_more_details.appendChild(alink_more_details_text);
+                                // alink.setAttribute("href","http://index.html");
+                                //alink_more_details.setAttribute('value',"ADD")
+                                
+                                alink_more_details.setAttribute('onclick',"add_member_in_table_addedmembers(); return false");
+                                alink_more_details.setAttribute('id',"add_member_btn");
+                                //alink_more_details.href = "more-detail.html?id="+id;
+                                
+                                // alink_more_details.href = "#table_addedmembers?id="+id;
+                                
+                                
+                                cell1.innerHTML = email.val();
+                                cell2.appendChild(alink_more_details);
+                                
+                                var hidden_field = document.createElement("INPUT");
+                                hidden_field.setAttribute("type","hidden");
+                                hidden_field.setAttribute("id","hidden_value");
+                                document.body.appendChild(hidden_field);
+                                
+                                
+                                document.getElementById('hidden_value').value = email.val();
+                                
+                                // }
+                                //document.getElementById('table_searchresults').deleteRow(table.rows.length - 1); 
+                            }  
                         }
+                    });
+                }
+                
+            });
+            
+            var table_rows = document.getElementById('table_addedmembers');
+            
+            for(var i = 0;i< table_rows.rows.length;i++)
+            {
+                if(table_rows.rows[i].cells[0].innerHTML == input_value){
+                    document.getElementById('add_member_btn').disabled= true;
+                    document.getElementById('table_searchresults').deleteRow(1);
+                }
+                
+            }
+            
+            
+            
+            databaseRef.on('value',function(snap){
+                
+                
+                if(snap.val() == null){
+                    window.alert("No such user exist");
+                }
+                else
+                {
+                    
+                }
+                
+                
+                
+                
+            });
+            
+        };
+        
+        document.getElementById('create_group').onclick =  function(){
+            var group_name = document.getElementById('group_name').value;
+            // databaseRef_group.child(group_name).on("value",function(snap){
+            //     //window.alert('Name already taken! Please try with some different name');
+            //     console.log('Name already taken');
+            // });
+            databaseRef_group.on('value',function(snap){
+                var group_names = snap.val();
+                var keys = Object.keys(group_names);
+                
+                for(var i = 0; i< keys.length;i++){
+                    if(group_name == keys[i])
+                    {
+                        alert('Same group name already exists! Please try again with different name');
+                        window.location = "contact-page.html";
+                        
                     }
                     else
                     {
-                        var row = table.insertRow(1);
-                        var cell1 = row.insertCell(0);
-                        var cell2 = row.insertCell(1);
+                        var group_description = document.getElementById('group_description').value;
+                        
+                        databaseRef_group.child(group_name).child('details').set({
+                            description : group_description,
+                            created_by : user.email
+                        });
                         
                         
-                        
-                        var alink_more_details = document.createElement("button");
-                        var alink_more_details_text = document.createTextNode('ADD');
-                        alink_more_details.appendChild(alink_more_details_text);
-                        // alink.setAttribute("href","http://index.html");
-                        //alink_more_details.setAttribute('value',"ADD")
-                        
-                        alink_more_details.setAttribute('onclick',"add_member_in_table_addedmembers(); return false");
-                        alink_more_details.setAttribute('id',"add_member_btn");
-                        //alink_more_details.href = "more-detail.html?id="+id;
-                        
-                        // alink_more_details.href = "#table_addedmembers?id="+id;
-                        
-                        
-                        cell1.innerHTML = email.val();
-                        cell2.appendChild(alink_more_details);
-                        
-                        var hidden_field = document.createElement("INPUT");
-                        hidden_field.setAttribute("type","hidden");
-                        hidden_field.setAttribute("id","hidden_value");
-                        document.body.appendChild(hidden_field);
-                        
-                        
-                        document.getElementById('hidden_value').value = email.val();
-                        
-                        // }
-                        //document.getElementById('table_searchresults').deleteRow(table.rows.length - 1); 
-                    }  
+                        var table = document.getElementById('table_addedmembers');
+                        // var row = table.insertRow(1);
+                        // var cell1 = row.insertCell(0);
+                        // cell1.innerHTML = user.email;
+                        for(var i=1;i<table.rows.length;i++){
+                            var value = table.rows[i].cells[0].innerHTML;
+                            var member = "member"+i;
+                            databaseRef_group.child(group_name).child('members').push({
+                                member : value 
+                            });
+                        }
+
+                        console.log('Member added');
+                        break;
+                    }
+                    
+                    console.log('In loop');
                 }
+                console.log('Out of loop');
+                // if(flag == 1){
+                //     alert('Same Name Exists! Please try again with different name');
+                //     return false;
+                // }
+                // else
+                // {
+                // var group_pic = document.getElementById('group_pic').value;
+                
+                
+                
             });
-        }
-        
-    });
-    
-    var table_rows = document.getElementById('table_addedmembers');
-    
-    for(var i = 0;i< table_rows.rows.length;i++)
-    {
-        if(table_rows.rows[i].cells[0].innerHTML == input_value){
-            document.getElementById('add_member_btn').disabled= true;
             
-            break;
-        }
-        
+            return false;
+            
+        };
+    }
+    else
+    {
+        console.log('Error');
     }
     
     
-    
-    databaseRef.on('value',function(snap){
-        
-        
-        if(snap.val() == null){
-            window.alert("No such user exist");
-        }
-        else
-        {
-            
-        }
-        
-        
-        
-        
-    });
-    
-    
-    
-    
-}
+});
+
+
+// function getusername(){
+
+
+
+
+
+// }
 
 
 
@@ -100,10 +177,10 @@ function add_member_in_table_addedmembers(){
     var row = table.insertRow(table.rows.length);
     var cell1 = row.insertCell(0);
     cell1.innerHTML = document.getElementById('hidden_value').value;
-
-
-
-
+    
+    
+    
+    
     // document.getElementById('add_member_btn').disabled= true;
     var table = document.getElementById("table_searchresults");
     table.deleteRow(1);
@@ -111,28 +188,6 @@ function add_member_in_table_addedmembers(){
 
 // var databaseRef = firebase.database().ref('groups');
 
-// function creategroup(){
-
-
-//     var username = sessionStorage.getItem('username');
-//     var group_pic = document.getElementById('group_pic').value;
-//     var group_name = document.getElementById('group_name').value;
-//     var group_description = document.getElementById('group_description').value;
-
-//     databaseRef.child(group_name).child('details').set({
-//         description : group_description,
-//         created_by : username
-//     });
-
-//     var table = document.getElementById('table_addedmembers');
-//     for(var i=1;i<table.rows.length;i++){
-//         var value = table.rows[i].cells[0].innerHTML;
-//         var member = "member"+i;
-//         databaseRef.child(group_name).child('members').push({
-//             member : value 
-//         });
-//     }
-
-
-
-// }
+function creategroup(){
+    
+}
