5cde5a72468f07188a990460aa5aa2ce8c724ab3
scorpionakki <alkesh302@gmail.com>
2018-12-06 00:51:48 +0530

    Was able to do it in Saved-images Buttt......
    Somehow the page is reloading on Store button and due to that the file
    isn't uploaded and not stored in database and can't be retrieved.

--- JS_Files_DB/saved-images.js
+++ JS_Files_DB/saved-images.js
@@ -1,37 +1,69 @@
-var selectedFile;
-function getFile(event){
-    selectedFile = event.target.files[0];
-}
-// document.getElementById('add_photo_to_storage').addEventListener('click',function(){
-//     alert('Hello');
-//     // var filename = selectedFile.name;
-//     // var storageRef = firebase.storage().ref('/REIG/'+filename);
-//     // var uploadTask = storageRef.put(selectedFile);
-
-//     // uploadTask.on('state_changed',function(snapshot){
-
-//     // },function(error){
-
-//     // },function(){
-//     //     var downloadURL = uploadTask.snapshot.downloadURL;
-//     //     console.log(downloadURL);
-//     // });
-
-//     return false;
-// },false);
-
-function uploadFile(){
-    var filename = selectedFile.name;
-    var storageRef = firebase.storage().ref('/REIG/'+filename);
-    var uploadTask = storageRef.put(selectedFile);
-    
-    uploadTask.on('state_changed',function(snapshot){
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
         
-    },function(error){
+        var tableRef = document.getElementById('img_table').getElementsByTagName('tbody')[0];
+        // Insert a row in the table at the last row
         
-    },function(){
-        var downloadURL = uploadTask.snapshot.downloadURL;
-        console.log(downloadURL);
-    });
+        var databaseRef = firebase.database().ref('users').child(user.uid).child('data_sharing');
+        databaseRef.on('value',function(snapshot_data_sharing){
+            var data_sharing = snapshot_data_sharing.val();
+            var data_sharing_keys = Object.keys(data_sharing);
+            for(var i=0;i<data_sharing_keys.length;i++){
+                databaseRef.child(data_sharing_keys[i]).child('imageURL').on('value',function(snapshot_imageURL){
+                    var newRow   = tableRef.insertRow(0);
+                    var imageCell = newRow.insertCell(0);
+                    var imageCellValue = document.createElement('img');
+                    imageCellValue.setAttribute('src',snapshot_imageURL.val());
+                    imageCellValue.setAttribute('height','40%');
+                    imageCellValue.setAttribute('width','40%');
+                    imageCell.appendChild(imageCellValue);
+                    //tableRef.deleteRow(data_sharing_keys.length);
+                    
+                })
+                
+            }
+        });
+        document.getElementById('file').onchange = function(event){
+            selectedFile = event.target.files[0];
+        }
+        
+        var profpicRef = firebase.database().ref('users/'+user.uid+'/data_sharing');
+        document.getElementById('upload_btn').onclick = function(){
 
-}
+            
+            var filename = selectedFile.name;
+            var storageRef = firebase.storage().ref('/'+user.uid+'/'+filename);
+            var uploadTask = storageRef.put(selectedFile);
+            
+            uploadTask.on('state_changed',function(snapshot){
+                
+            },function(error){
+                
+            },function(){
+                uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL){
+                    profpicRef.push({
+                        imageURL : downloadURL
+                    });
+                    // imgsrc = downloadURL;
+                    // document.getElementById('put_image').src = imgsrc;
+                } );
+                
+                window.alert('Uploaded');
+                
+                //document.getElementById('put_image').src = imgsrc;
+                //window.alert(imgsrc);
+                
+            });
+
+            return true;
+        }
+        
+    } else {
+        // No user is signed in.
+        alert('No user is currently signed in');
+        window.location.replace = "/login.html";
+    }
+});
+
+
