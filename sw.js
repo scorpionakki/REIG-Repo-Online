219348936c162b3d917ea2c8520a56a326f0b44a
Bhargav Borse <bhargavborse4898@gmail.com>
2018-12-08 23:47:06 +0530

    service worker part done

--- sw.js
+++ sw.js
@@ -1,21 +1,11 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js')
-    .then(registration => {
-      console.log('Service Worker is registered', registration);
-    })
-    .catch(err => {
-      console.error('Registration failed:', err);
-    });
-  });
-}
-
 self.addEventListener('install', event => {
   console.log('Service worker installing...');
-  self.skipWaiting();
   // Add a call to skipWaiting here
 });
-
+self.skipWaiting();
 self.addEventListener('activate', event => {
   console.log('Service worker activating...');
+});
+self.addEventListener('fetch', event => {
+  console.log('Fetching:', event.request.url);
 });
