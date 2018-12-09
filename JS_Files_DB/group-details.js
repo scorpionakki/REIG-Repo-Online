e55d14db9265a9444456e71fe8d9bf9676623567
scorpionakki <alkesh302@gmail.com>
2018-12-10 03:49:40 +0530

    Now every group page can check whether the user accessing the page is a member of the same group or not

--- JS_Files_DB/group-details.js
+++ JS_Files_DB/group-details.js
@@ -6,6 +6,74 @@ firebase.auth().onAuthStateChanged(function(user) {
         var url = new URL(url_string);
         var grp_name = url.searchParams.get("name");
         var grp_id = url.searchParams.get("id");
+        
+        var groupRef = firebase.database().ref('groups');
+        groupRef.on('value',function(groupids_snapshot){
+            var groupids = groupids_snapshot.val();
+            var groupidskey = Object.keys(groupids);
+            
+            for(var i=0;i<groupidskey.length;i++){
+                if(grp_id == groupidskey[i]){
+                    groupRef.child(groupidskey[i]).on('value',function(groupname_snapshot){
+                        var groupname = groupname_snapshot.val();
+                        var groupnamekeys = Object.keys(groupname);
+                        if(grp_name != groupnamekeys[0]){
+                            alert('Problem with the Name');
+                            window.location = "index.html";
+                        }
+                        else
+                        {
+                            groupRef.child(groupidskey[i]).child(groupnamekeys[0]).child('members').on('value',function(memberids_snapshot){
+                                var membersids = memberids_snapshot.val();
+                                var membersidskeys = Object.keys(membersids);
+                                flag = 0;
+                                for(var j=0;j<membersidskeys.length;j++){
+                                    groupRef.child(groupidskey[i]).child(groupnamekeys[0]).child('members').child(membersidskeys[j]).on('value',function(membersemail_snapshot){
+                                        var membersemail = membersemail_snapshot.val();
+                                        
+                                        if(user.email != membersemail.member){
+                                            flag = flag + 1; 
+                                            
+                                        }
+                                        
+                                        //console.log(membersemail.member);
+                                        // if(user.email == membersemail.member){
+                                        //     flag == 1;
+                                        // }
+                                        // console.log(membersemail.member);
+                                        // console.log(user.email);
+                                        // console.log(flag);
+                                        // if(flag != 1){
+                                        //     alert('Not a current member');
+                                        // }
+                                    });
+                                }
+                                console.log(flag);
+                                flag_call(flag,membersidskeys.length);
+                                function flag_call(flag,membersidskeysvalue){
+                                    if(flag == membersidskeysvalue){
+                                        alert('Not a member');
+                                        window.location = "index.html";
+                                    }
+                                }
+                                // for(var j=0;j<membersidskeys.length;j++){
+                                //     if(user.email == membersidskeys[i].member){
+                                //         flag == 1;
+                                //     }
+                                // }
+                                
+                                // if(flag != 1){
+                                //     alert('Not current member of this group');
+                                
+                                // }
+                            });
+                        }
+                    });
+                }
+            }
+            
+        });
+        
         document.getElementById('redirect_group').href = 'group.html?id='+grp_id+'&name='+grp_name;
         firebaseRef.child(grp_id).child(grp_name).child('details').on('value',function(group_details_snapshot){
             var group_details = group_details_snapshot.val();
--- JS_Files_DB/group-members.js
+++ JS_Files_DB/group-members.js
@@ -8,6 +8,72 @@ firebase.auth().onAuthStateChanged(function(user) {
         var url = new URL(url_string);
         var grp_name = url.searchParams.get("name");
         var grp_id = url.searchParams.get("id");
+        var groupRef = firebase.database().ref('groups');
+        groupRef.on('value',function(groupids_snapshot){
+            var groupids = groupids_snapshot.val();
+            var groupidskey = Object.keys(groupids);
+            
+            for(var i=0;i<groupidskey.length;i++){
+                if(grp_id == groupidskey[i]){
+                    groupRef.child(groupidskey[i]).on('value',function(groupname_snapshot){
+                        var groupname = groupname_snapshot.val();
+                        var groupnamekeys = Object.keys(groupname);
+                        if(grp_name != groupnamekeys[0]){
+                            alert('Problem with the Name');
+                            window.location = "index.html";
+                        }
+                        else
+                        {
+                            groupRef.child(groupidskey[i]).child(groupnamekeys[0]).child('members').on('value',function(memberids_snapshot){
+                                var membersids = memberids_snapshot.val();
+                                var membersidskeys = Object.keys(membersids);
+                                flag = 0;
+                                for(var j=0;j<membersidskeys.length;j++){
+                                    groupRef.child(groupidskey[i]).child(groupnamekeys[0]).child('members').child(membersidskeys[j]).on('value',function(membersemail_snapshot){
+                                        var membersemail = membersemail_snapshot.val();
+                                        
+                                        if(user.email != membersemail.member){
+                                            flag = flag + 1; 
+                                            
+                                        }
+                                        
+                                        //console.log(membersemail.member);
+                                        // if(user.email == membersemail.member){
+                                        //     flag == 1;
+                                        // }
+                                        // console.log(membersemail.member);
+                                        // console.log(user.email);
+                                        // console.log(flag);
+                                        // if(flag != 1){
+                                        //     alert('Not a current member');
+                                        // }
+                                    });
+                                }
+                                console.log(flag);
+                                flag_call(flag,membersidskeys.length);
+                                function flag_call(flag,membersidskeysvalue){
+                                    if(flag == membersidskeysvalue){
+                                        alert('Not a member');
+                                        window.location = "index.html";
+                                    }
+                                }
+                                // for(var j=0;j<membersidskeys.length;j++){
+                                //     if(user.email == membersidskeys[i].member){
+                                //         flag == 1;
+                                //     }
+                                // }
+                                
+                                // if(flag != 1){
+                                //     alert('Not current member of this group');
+                                
+                                // }
+                            });
+                        }
+                    });
+                }
+            }
+            
+        });
         
         document.getElementById('loading_gif').style.visibility = 'hidden';
         document.getElementById('group_redirect').href = "group.html?id="+grp_id+"&name="+grp_name;
